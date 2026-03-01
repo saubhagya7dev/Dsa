8d6e122269f6dff63cc7242484ffd53379b02a94
saubhagya <saubhagya7developer@gmail.com>
2026-03-02 02:47:04 +0530

    more ab linked list

--- linkedList.cpp
+++ linkedList.cpp
@@ -6,28 +6,26 @@ class Node{
     int data;
     Node* next;
 
-    Node(int data){
-        this->data = data;
-        this->next = NULL;
-    }
-
     public:
     Node(int data1,Node* next1){
-        this->data = data1;
-        this->next = next1;
+        data = data1;
+        next = next1;
+    }
+    public: // this constructor was created so that we don't have to pass the next pointer every time we create a new node, since it will be nullptr by default
+    Node(int data1){
+        data = data1;
+        next = nullptr;
     }
         
 };
-
-
 
 int main (){
 
     vector<int> v = {10,2,3,4,5};
 
-    Node* y = new Node(v[0],nullptr);
-    cout<<y->data<<endl;
-    cout<<y<<endl;
+    Node* y = new Node(v[0]);
+    cout<<y->data<<endl; // prints the data of the first node, which is 10
+    cout<<y<<endl; // prints the memory address of the first node, which is a pointer to the node object in memory
     
     return 0;
 
